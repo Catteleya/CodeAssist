bf3997e31cefe7b3e25b9d3f652ff6305b63eb01
EclipseTheDev <115270371+EclipseTheDev@users.noreply.github.com>
2023-01-01 07:50:38 -0500

    Update index.js

--- text-editor/index.js
+++ text-editor/index.js
@@ -13,3 +13,10 @@ const sub = document.getElementById('example-btn');
 sub.addEventListener('click', () => {
   window.open('https://example.com', '_blank');
 });
+
+    function featured{
+      alert ("We are still developing this + no one has submitted to be featured :c");
+    }
+    function beFeat{
+      alert ("We are trying to make one, sorry bestieeeeee");
+    }
