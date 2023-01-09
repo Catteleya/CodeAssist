f756224539627ac854bc1e7ce72e296db272fedd
EclipseTheDev <115270371+EclipseTheDev@users.noreply.github.com>
2023-01-09 09:47:53 -0500

    Update index.js

--- text-editor/index.js
+++ text-editor/index.js
@@ -1,5 +1,7 @@
 document.getElementById("heading").innerHTML =
   localStorage["title"] || "Title"; // default text
+document.getElementById("heading-sub-text").innerHTML =
+  localStorage["title-sub-text"] || "Heading Sub-Text"; // default text
 document.getElementById("content").innerHTML =
   localStorage["text"] || "Subtext [inf]"; // default text
 
@@ -13,6 +15,7 @@ function beFeat() {
 setInterval(function() {
   // fuction that is saving the innerHTML of the div
   localStorage["title"] = document.getElementById("heading").innerHTML; // heading div
+  localStorage["heading-sub-text"] = document.getElementById("heading-sub-text").innerHTML
   localStorage["text"] = document.getElementById("content").innerHTML; // content div
 }, 1000);
 const sub = document.getElementById('example-btn');
