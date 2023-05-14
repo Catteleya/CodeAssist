1dbd3874b3879fd0cd48bd7a6d1f9ad58d113c91
Catteleya <115270371+Catteleya@users.noreply.github.com>
2023-05-14 18:07:29 -0400

    Create index.js

---
+++ TBYW/discord/index.js
@@ -0,0 +1,7 @@
+<html>
+<head>
+<link rel="stylesheet" href="./styles.css">
+<meta http-equiv="refresh" content="0; URL=https://discord.gg/MMAQWsnXZh">
+<link rel="canonical" href="https://discord.com/invite/Fwt4mUHvQQ">
+</head>
+</html>
